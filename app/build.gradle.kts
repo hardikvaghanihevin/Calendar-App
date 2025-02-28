f26a027529fe00f20abdaee6af586d1e42dc9c6e
Hardik <hardikvaghanihevin@gmail.com>
2025-02-28 14:08:42 +0530

    -ready-to-test5 -> to -> dev-005 -remove unused library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,10 +86,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.ui.graphics.android)
-    implementation(libs.legacy.support.v4)
     implementation(libs.preference)
 
     // Hilt DI
@@ -147,7 +144,7 @@ dependencies {
     implementation ("androidx.fragment:fragment-ktx:1.6.1")
 
     // Material Design Components
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("com.google.android.material:material:1.12.0")//1.9.0
 
     // Firebase dependencies
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
